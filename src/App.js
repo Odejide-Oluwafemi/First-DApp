082836a04c075596e990e60c11e680ac1418b345
DESKTOP-8IS7CJH\SATISFY <oluwatosinodejide121@gmail.com>
2025-07-17 08:17:56 +0100

    Added New App (Meme Generator)

--- src/App.js
+++ src/App.js
@@ -4,9 +4,14 @@ import TodoApp from "./MyApps/00. TodoApp/TodoApp.js";
 import TravelJournal from "./MyApps/03. TravelJournalApp/TravelJournal.js";
 import AIRecipeApp from "./MyApps/04. AIRecipeApp/AIRecipeApp.js";
 import SoundPadApp from "./MyApps/05. SoundPadApp/SoundPadApp.jsx";
+import MemeGeneratorApp from "./MyApps/06. MemeGeneratorApp/MemeGeneratorApp.jsx";
+
 function App() {
   return (
     <>
+      <MemeGeneratorApp/>
+      <hr/>
+      
       <AIRecipeApp/>
       <hr/>
       
---
+++ src/MyApps/06. MemeGeneratorApp/Header.jsx
@@ -0,0 +1,13 @@
+import "./styles/header.css";
+
+export default function Header()
+{
+  return(
+    <header className="meme-generator-header">
+      <span>
+        <img src="https://pngimg.com/d/trollface_PNG31.png"/>
+        <h1>Meme Generator</h1>
+      </span>
+    </header>
+  );
+}
